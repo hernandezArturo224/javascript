14e67fbda03652ab04c01db4d6afff1dff717879
ArturoHdez <hernandezarturo224@gmail.com>
2021-04-26 18:13:59 +0200

    Commit con primer ejercicio

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function muestraPar(){
+	var sign = prompt("Introduce un numero para saber si es par o impar");
+
+	if(sign%2 === 0){
+		alert("El numero es par");
+	}else{
+		alert("El numero es impar");
+	}
+}
